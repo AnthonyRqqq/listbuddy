f032b6dacc4052527068156652b9845c1304fce1
Anthony Rosati <anthony_rosati@yahoo.com>
2025-11-08 12:49:22 -0600

    front page general framework set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,35 @@
+import { Outlet, useLocation } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import Nav from "./Components/Nav";
 
-function App() {
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
+
+export default function App() {
+  const currPage = useLocation().pathname;
+
   return (
-    <>potato
-    </>
-  )
-}
+    // ApolloProvider wrapper enables access to ApolloClient from anywhere in program
+    <ApolloProvider client={client}>
+      {currentPage === "/" ? (
+        <div className="homepage-navigation">
+          <Navigation />
+        </div>
+      ) : (
+        <>
+          <header className="header">
+            <h3>
+              <Navigation />
+            </h3>
+          </header>
 
-export default App
+          <main className="main">
+            <Outlet />
+          </main>
+        </>
+      )}
+    </ApolloProvider>
+  );
+}
